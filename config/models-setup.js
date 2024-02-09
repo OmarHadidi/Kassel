fc9a22eeef4f9a22e00a75d2c640b1ae3414b894
Omar Hadidi <omarhadidi20062006@gmail.com>
2024-02-09 15:40:59 +0200

    connected to gcloud db

--- config/models-setup.js
+++ config/models-setup.js
@@ -1,7 +1,16 @@
 const { Sequelize } = require("sequelize");
 const { setupModels } = require("../models");
 
-const sequelize = new Sequelize(process.env.DB_URI);
+// const sequelize = new Sequelize(process.env.DB_URI);
+const { DB_PORT, DB_USER, DB_HOST, DB_PASSWORD, DB_DATABASE } = process.env;
+const sequelize = new Sequelize({
+    dialect: "mysql",
+    port: DB_PORT,
+    username: DB_USER,
+    host: DB_HOST,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+});
 
 module.exports = {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -66,7 +66,16 @@ async function syncModels(sequelize) {
 }
 
 // To run here
-const sequelize = new Sequelize(process.env.DB_URI);
+// const sequelize = new Sequelize(process.env.DB_URI);
+const { DB_PORT, DB_USER, DB_HOST, DB_PASSWORD, DB_DATABASE } = process.env;
+const sequelize = new Sequelize({
+    dialect: "mysql",
+    port: DB_PORT,
+    username: DB_USER,
+    host: DB_HOST,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+});
 sequelize.authenticate().then(async () => {
     await syncModels(sequelize);
 });
